35850201548f40ac65ee3bd6b595ad4126d59353
Martin Procházka <martin.prochazka@protonmail.com>
2025-05-01 13:36:50 +0200

    Implemented CLI for single player game, closes #24 - this also improves the redraw behavior so that its more accurate

--- src/game/game_sp.cpp
+++ src/game/game_sp.cpp
@@ -4,18 +4,64 @@
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/event.hpp>
 
+#include "argparse/argparse.hpp"
 #include "engine/game.hpp"
 #include "game/renderer.hpp"
 
 using namespace ftxui;
 using namespace std::chrono_literals;
 
+struct Configuration
+{
+    uint64_t seed = Clock::now().time_since_epoch().count();
+    uint32_t goal;
+    uint32_t tick_length;
+    uint32_t frame_length;
+};
+
+auto parse(int argc, char** argv)
+{
+    auto program = argparse::ArgumentParser("tetriz", "0.0.0");
+    auto configuration = Configuration{};
+
+    program.add_argument("--seed")
+        .help("game seed, ommit for random seed")
+        .scan<'i', uint64_t>()
+        .store_into(configuration.seed);
+
+    program.add_argument("--goal")
+        .help("number of lines to clear to win the game")
+        .default_value<uint32_t>(40)
+        .scan<'i', uint32_t>()
+        .nargs(1)
+        .store_into(configuration.goal);
+
+    program.add_argument("--tick-length")
+        .help("length of each tick in milliseconds")
+        .default_value<uint32_t>(1000)
+        .scan<'i', uint32_t>()
+        .nargs(1)
+        .store_into(configuration.tick_length);
+
+    program.add_argument("--frame-length")
+        .help("maximum length of a single frame")
+        .default_value<uint32_t>(33)
+        .scan<'i', uint32_t>()
+        .nargs(1)
+        .store_into(configuration.frame_length);
+
+    program.parse_args(argc, argv);
+
+    return configuration;
+}
 
 auto main(int argc, char** argv) -> int
 {
+    const auto config = parse(argc, argv);
     const auto time_begin = Clock::now();
+
     auto time = std::chrono::duration_cast<Duration>(Clock::now() - time_begin);
-    auto game = tetriz::Game(4);
+    auto game = tetriz::Game(config.seed, config.goal);
     auto game_renderer = make_board_renderer(game, time);
     auto event_listener = CatchEvent(game_renderer, [&](const Event& e) {
         if (game.state() != tetriz::State::Playing)
@@ -42,22 +88,30 @@ auto main(int argc, char** argv) -> int
     auto screen = ScreenInteractive::FitComponent();
     screen.SetCursor(ftxui::Screen::Cursor(0, 0, Screen::Cursor::Hidden));
 
-    auto running = true;
-    auto game_worker = std::jthread([&]{
-        auto next_tick = std::chrono::steady_clock::now() + 1s;
+    auto running = std::atomic<bool>(true);
+
+    const auto tick_ms = std::chrono::milliseconds(config.tick_length);
+    const auto frame_ms = std::chrono::milliseconds(config.frame_length);
+    const auto game_worker = std::jthread([&] {
+        auto next_tick = Clock::now() + tick_ms;
         while (running && game.state() == tetriz::State::Playing)
         {
-            time = std::chrono::duration_cast<Duration>(Clock::now() - time_begin);
+            const auto time_now = Clock::now();
+            time = normalize_duration(time_now - time_begin);
 
-            if (std::chrono::steady_clock::now() >= next_tick)
+            if (time_now >= next_tick)
             {
                 game.tick();
-                next_tick += 1s;
+                next_tick += tick_ms;
             }
 
             screen.PostEvent(Event::Custom);
-            std::this_thread::sleep_for(73ms);
+
+            const auto next_frame = time_now + frame_ms;
+            std::this_thread::sleep_until(std::min(next_frame, next_tick));
         }
+
+        screen.Exit();
     });
 
     screen.Loop(event_listener);
--- src/util/time.hpp
+++ src/util/time.hpp
@@ -6,3 +6,9 @@ using Clock = std::chrono::high_resolution_clock;
 using TimePoint = std::chrono::time_point<Clock>;
 using Duration = std::chrono::duration<int32_t, std::centi>;
 
+template <typename T, typename R>
+auto normalize_duration(std::chrono::duration<T, R> value)
+{
+    return std::chrono::duration_cast<Duration>(value);
+}
+
